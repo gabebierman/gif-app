f1d057d1b1e658dd4d9b9a7ba73567cdb266a813
gabebierman <gabebierman@gmail.com>
2022-11-02 18:53:19 -0500

    fixed server loading issue

--- server/config/database.config.js
+++ server/config/database.config.js
@@ -2,7 +2,7 @@ import mysql from "mysql";
 import util from "util";
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
+    user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_SCHEMA,
     port: process.env.DB_PORT,
--- server/middleware/validateUserData.middleware.js
+++ server/middleware/validateUserData.middleware.js
@@ -1,6 +1,6 @@
 export default function validateData(req, res, next) {
     const { username, password } = req.body;
-    if (!username || username.length < 4 || username.length > 20) {
+    if (!username || username.length < 4 || 20 < username.length) {
         return res.send({ error: "Username does not meet requirements", success: false });
     }
     if (!password || password.length < 8 || password.length > 20) {
