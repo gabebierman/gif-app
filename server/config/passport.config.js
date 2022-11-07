e8cadbf6931e1d0e192b612ff6571a92d69421ef
gabebierman <gabebierman@gmail.com>
2022-11-07 14:40:59 -0600

    adds passport config

--- server/config/passport.config.js
+++ server/config/passport.config.js
@@ -1,1 +1,34 @@
 //TODO set up passport configuration
+import { Strategy } from "passport-jwt";
+import passport from "passport";
+
+const cookieExtractor = (req) => {
+    const token = null;
+    if (req && req.cookies) {
+        token = req.cookies["auth"];
+    }
+    return token;
+};
+
+const jwtOptions = {
+    jwtFromRequest: cookieExtractor,
+    secretOrKey: process.env.SECRET_KEY,
+};
+
+passport.use(
+    new Strategy(jwtOptions, async (payload, done) => {
+        const { user_id } = payload;
+        try {
+            const [user] = await query(
+                "SELECT username , uuid , id FROM user WHERE user.id = ?",
+                [user_id]
+            );
+            if (!user) return done(null, false, "No user found");
+            return done(null, user);
+        } catch (err) {
+            return done(err, false, "Something went wrong , could not authenticate 🤷‍♂️");
+        }
+    })
+);
+
+export default passport;
